34347c741240c7ac3b579cc21e0dfdbe8d54e593
Shreyansh Singh <lab.shreyansh.singh@gmail.com>
2026-08-27 14:33:21 +0530

    Add logic to group anagrams using a map
    Implemented a mapping for grouped anagrams in the function.

--- Data Structures & Algorithms/anagram-groups/submission-0.js
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -8,6 +8,7 @@ class Solution {
         let map = {};
         for(let i = 0; i < strs.length;i++){
             let x = strs[i].split("").sort().join("")
+            //if function check for the key to be same and then later add the item itself to a seperate list in the array, and if the key matches it skips the if fucntion and dirfectly add the list item to the array.
             if(!map[x]){
                 map[x] = [];
             }
